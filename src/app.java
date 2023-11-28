629e2baf1ae73c94cbbd133c9e295add5990bbc5
arisia <20231331@daejin.ac.kr>
2023-11-28 16:39:10 +0900

    add first logic

--- src/app.java
+++ src/app.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
 public class app {
     public static void main(String[] args){
-
+        boolean isexit = false;
+        do{
+            System.out.println("-------- 메뉴 --------");
+            System.out.println("1. 원격 데스크탑 서버 실행");
+            System.out.println("2. 원격 데스크탑 클라이언트 실행");
+            System.out.println("3. 환경 설정");
+            System.out.println("4. 종료");
+            Scanner sc = new Scanner(System.in);
+            System.out.print("메뉴를 선택하세요 : ");
+            int menu = sc.nextInt();
+            switch(menu){
+                case 1 -> {
+                    System.out.println("원격 데스크탑 서버 실행");
+                }
+                case 2 -> {
+                    System.out.println("원격 데스크탑 클라이언트 실행");
+                }
+                case 3 -> {
+                    System.out.println("환경 설정");
+                }
+                case 4 -> {
+                    System.out.println("종료");
+                    isexit = true;
+                }
+            }
+        }while (!isexit);
     }
 }
