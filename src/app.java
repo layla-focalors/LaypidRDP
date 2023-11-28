668bd0764a1c17078e97c825546deeafc68bb3ae
arisia <20231331@daejin.ac.kr>
2023-11-28 16:48:45 +0900

    Add ip detector

--- src/app.java
+++ src/app.java
@@ -1,13 +1,22 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Scanner;
+
+class Server {
+    final int width = 1920;
+    final int height = 1080;
+}
+
 public class app {
-    public static void main(String[] args){
+    public static void main(String[] args) throws UnknownHostException {
         boolean isexit = false;
         do{
             System.out.println("-------- 메뉴 --------");
             System.out.println("1. 원격 데스크탑 서버 실행");
             System.out.println("2. 원격 데스크탑 클라이언트 실행");
             System.out.println("3. 환경 설정");
-            System.out.println("4. 종료");
+            System.out.println("4. 정보 확인");
+            System.out.println("5. 종료");
             Scanner sc = new Scanner(System.in);
             System.out.print("메뉴를 선택하세요 : ");
             int menu = sc.nextInt();
@@ -22,6 +31,13 @@ public class app {
                     System.out.println("환경 설정");
                 }
                 case 4 -> {
+                    System.out.println("------ 정보 확인 ------");
+                    InetAddress local;
+                    local = InetAddress.getLocalHost();
+                    String ipd = local.getHostAddress();
+                    System.out.println("접속한 디바이스의 IP : " + ipd);
+                }
+                case 5 -> {
                     System.out.println("종료");
                     isexit = true;
                 }
